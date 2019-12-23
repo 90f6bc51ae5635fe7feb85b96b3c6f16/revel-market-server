55ad657bce9e10fc33aa36dda2d5bb8f5222b32e
Maxky2208 <maxky2208@gmail.com>
2019-12-23 09:57:31 +0700

    ++

--- models/BaseModel.js
+++ models/BaseModel.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     connectionimit: 100,
-    host: '192.168.0.129',
+    host: 'localhost',
     user: 'admin',
     port: 3306, 
     password: 'root123456',
