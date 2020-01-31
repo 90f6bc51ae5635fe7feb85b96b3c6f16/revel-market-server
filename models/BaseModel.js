dfa81311958ace567418cc0c0c87f8cb012ab4ef
Maxky2208 <maxky2208@gmail.com>
2020-01-31 10:35:00 +0700

    Update BaseModel.js

--- models/BaseModel.js
+++ models/BaseModel.js
@@ -1,14 +1,25 @@
 var mysql = require('mysql');
 
+// var connection = mysql.createConnection({
+//     connectionimit: 100,
+//     host: 'localhost',
+//     user: 'root',
+//     port: 3306, 
+//     password: 'root123456',
+//     database: 'revel-market_db',
+//     multipleStatements: true
+// }); 
+
 var connection = mysql.createConnection({
     connectionimit: 100,
-    host: 'localhost',
-    user: 'root',
-    port: 3306, 
+   host: 'database-rvscs.cdaloohdepaj.ap-southeast-1.rds.amazonaws.com',
+    user: 'admin',
+    port: 3306,
     password: 'root123456',
     database: 'revel-market_db',
     multipleStatements: true
-}); 
+});
+
 
 connection.connect();
 
